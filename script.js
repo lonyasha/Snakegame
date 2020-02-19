f9b31b8aa74f2916a9d2fc222f3fa69619f1feaf
Ilona <ilona.gimaeva@gmail.com>
2020-02-19 13:33:48 +0100

    master

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const ROWS = 10
 const COLUMNS = 10
-const COOLDOWN = 250
+const START_COOLDOWN = 250
+const LEVEL_COOLDOWN = 15
 
 const CELL_SIZE = 50
 const CELL_MARGIN = 3
@@ -16,40 +17,90 @@ const context = canvas.getContext('2d')
 canvas.width = CELL_SIZE * COLUMNS + (COLUMNS - 1) * CELL_MARGIN + 2 * GAME_PADDING
 canvas.height = CELL_SIZE * ROWS + (ROWS - 1) * CELL_MARGIN + 2 * GAME_PADDING
 
-const map = creatGameMap(COLUMNS, ROWS)
+let map = creatGameMap(COLUMNS, ROWS)
 
 getRandomFreeCell(map).food = true
 
-const snake = [getRandomFreeCell(map)]
-snake[0].snake = true
+const cell = getRandomFreeCell(map)
+let snake = [cell]
 
-let snakeDirect = 'top'
+cell.snake = true
+
+let snakeDirect = 'up'
+let nextSnakeDirect = 'up'
+let cooldown = START_COOLDOWN
 
 requestAnimationFrame(loop)
 
 let prevTick = 0
+let play = true
 
-function loop (timestamp) {
+function loop(timestamp) {
   requestAnimationFrame(loop)
+
   clearCanvas()
-  if (prevTick + COOLDOWN <= timestamp) {
-    moveSnake()
+
+  if (prevTick + cooldown <= timestamp && play) {
     prevTick = timestamp
+
+    snakeDirect = nextSnakeDirect
+    moveSnake()
+
+    const head = snake[0]
+    const tail = snake[snake.length - 1]
+
+    if (head.food) {
+      head.food = false
+      snake.push(tail)
+
+      getRandomFreeCell(map).food = true
+      cooldown += LEVEL_COOLDOWN
+    } 
+    else {
+      let isEnd = false
+      for (let i = 1; i < snake.length; i++) {
+        if (snake[i] === snake[0]) {
+          isEnd = true
+          break
+        }
+      }
+      if (isEnd) {
+        play = false
+      }
+    }  
   }
+
   drawGameMap(map)
+  showState()
+
+  if (!play) {
+    drawPaused()
+  }
 }
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
   if (event.key === 'ArrowUp') {
-    snakeDirect = 'up'
-  }
-  if (event.key === 'ArrowDown') {
-    snakeDirect = 'down'
+    if (snake.length === 1 || snakeDirect === "left" || snakeDirect === "right") {
+      nextSnakeDirect = 'up'
+    }
+  } else if (event.key === 'ArrowDown') {
+    if (snake.length === 1 || snakeDirect === "left" || snakeDirect === "right") {
+      nextSnakeDirect = 'down'
+    }
   }
   if (event.key === 'ArrowLeft') {
-    snakeDirect = 'left'
+    if (snake.length === 1 || snakeDirect === "up" || snakeDirect === "down") {
+      nextSnakeDirect = 'left'
+    }
   }
   if (event.key === 'ArrowRight') {
-    snakeDirect = 'right'
+    if (snake.length === 1 || snakeDirect === "up" || snakeDirect === "down") {
+      nextSnakeDirect = 'right'
+    }
+  } else if (event.key === 'Enter') {
+    if (play) {
+      return
+    }
+    init()
   }
 })
--- snake.js
+++ snake.js
@@ -1,17 +1,17 @@
-function drawRect (param) {
+function drawRect(param) {
   context.beginPath()
   context.rect(param.x, param.y, param.width, param.height)
   context.fillStyle = param.fillColor
   context.fill()
 }
 
-function clearCanvas () {
+function clearCanvas() {
   context.clearRect(0, 0, canvas.width, canvas.height)
 }
 
-function creatGameMap (columns, rows) {
+function creatGameMap(columns, rows) {
   const map = []
-  for(let x = 0; x < columns; x++) {
+  for (let x = 0; x < columns; x++) {
     const row = []
     for (let y = 0; y < rows; y++) {
       row.push({
@@ -26,7 +26,7 @@ function creatGameMap (columns, rows) {
   return map
 }
 
-function getRandomFreeCell (map) {
+function getRandomFreeCell(map) {
   const freeCells = []
   for (const cell of map.flat()) {
     if (cell.snake || cell.food) {
@@ -38,7 +38,7 @@ function getRandomFreeCell (map) {
   return freeCells[index]
 }
 
-function drawGameMap (map) {
+function drawGameMap(map) {
   for (const cell of map.flat()) {
     const param = {
       x: GAME_PADDING + cell.x * (CELL_SIZE + CELL_MARGIN),
@@ -57,20 +57,19 @@ function drawGameMap (map) {
   }
 }
 
-function getCell (x, y) {
+function getCell(x, y) {
   if (x < 0) {
     x += COLUMNS
   }
-if (x >= COLUMNS) {
-  x -= COLUMNS
-}
-
-if (y < 0) {
-  y += ROWS
-}
-if (y >= ROWS) {
-  y -= ROWS
-}
+  if (x >= COLUMNS) {
+    x -= COLUMNS
+  }
+  if (y < 0) {
+    y += ROWS
+  }
+  if (y >= ROWS) {
+    y -= ROWS
+  }
 
   for (const cell of map.flat()) {
     if (cell.x === x && cell.y === y) {
@@ -79,17 +78,17 @@ if (y >= ROWS) {
   }
 }
 
-function moveSnake () {
+function moveSnake() {
+  for (let i = snake.length - 1; i > 0; i--) {
+    snake[i] = snake[i - 1]
+  }
   if (snakeDirect === 'left') {
     snake[0] = getCell(snake[0].x - 1, snake[0].y)
-  }
-  else if (snakeDirect === 'right') {
+  } else if (snakeDirect === 'right') {
     snake[0] = getCell(snake[0].x + 1, snake[0].y)
-  }
-  else if (snakeDirect === 'up') {
+  } else if (snakeDirect === 'up') {
     snake[0] = getCell(snake[0].x, snake[0].y - 1)
-  }
-  else if (snakeDirect === 'down') {
+  } else if (snakeDirect === 'down') {
     snake[0] = getCell(snake[0].x, snake[0].y + 1)
   }
   for (const cell of map.flat()) {
@@ -98,4 +97,41 @@ function moveSnake () {
   for (const cell of snake) {
     cell.snake = true
   }
+}
+
+function showState() {
+  context.font = "30px Roboto"
+  context.fillStyle = "black"
+  context.textAlign = "left"
+  context.fillText(`Cooldown: ${cooldown}`, 10, 50)
+  context.fillText(`Points: ${snake.length * 5}`, 10, 100)
+}
+
+function drawPaused() {
+  context.beginPath()
+  context.rect(0, 0, canvas.width, canvas.height)
+  context.fillStyle = 'rgba(255, 255, 255, 0.7)'
+  context.fill()
+
+  context.font = "60px Roboto"
+  context.fillStyle = "black"
+  context.textAlign = "center"
+  context.fillText(`Your points: ${snake.length * 5}`, canvas.width / 2, canvas.height / 2)
+  context.font = "40px Roboto"
+  context.fillText("Press enter to continue", canvas.width / 2, canvas.height / 2 + 50)
+}
+
+function init() {
+  map = creatGameMap(COLUMNS, ROWS)
+  getRandomFreeCell(map).food = true
+
+  const cell = getRandomFreeCell(map)
+  snake = [cell]
+
+  cell.snake = true
+
+  snakeDirect = 'up'
+  nextSnakeDirect = 'up'
+  play = true
+  cooldown = START_COOLDOWN
 }
